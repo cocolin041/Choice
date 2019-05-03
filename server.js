fd9f3f7e3fc1e0eb1c7eb6585054966c2cb55888
Sih-Yi Lin <sihyiyl2@illinois.edu>
2019-05-02 23:44:36 -0500

    test deploy

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/public')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    res.sendfile(path.join(__dirname = 'client/public/index.html'));
   })
 }
 //build mode
